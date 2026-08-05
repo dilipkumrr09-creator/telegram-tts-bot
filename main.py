571660f7d3ef43c66f5500e432ae631b69bdc182
dilipkumrr09-creator <dilipkumrr09@gmail.com>
2026-08-05 08:24:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'Bot is alive!'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+Thread(target=run).start()
+import os
 import asyncio
 import re
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
